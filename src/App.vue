c8c4b85ec304ef53a40b0d40b53c052b1a21d7c8
Maxime Amini <maximeamini1@gmail.com>
2023-03-16 11:30:55 +0100

    services section

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
     <Nav />
     <div class="pl-16 lg:pr-8 bg-[#1E1F21] text-white ">
+        <!-- photo -->
         <Photo />
+        <!-- Pages -->
         <div class="lg:min-h-screen w-full pt-4 pl-8 lg:pl-96">
-
             <About />
             <hr class="border-gray-600" />
-
+            <Services />
+            <hr class="border-gray-600" />
         </div>
     </div>
 </template>
@@ -15,6 +17,7 @@
 import Nav from "./components/nav/Nav.vue";
 import Photo from "./components/nav/Photo.vue";
 import About from "./components/pages/AboutView.vue";
+import Services from "./components/pages/ServicesView.vue";
 
 import {
     onMounted
---
+++ src/components/pages/ServicesView.vue
@@ -0,0 +1,74 @@
+<template>
+    <!-- What i do -->
+    <div class="pt-6 flex flex-wrap" id="services">
+        <h1 class="mb-4 text-4xl w-full" style="font-family: 'Barnacle Boy', sans-serif;" data-aos="flip-up" data-aos-duration="1000">My services </h1>
+
+        <div class="flex flex-wrap">
+            <div class="sm:w-1/2 p-4" data-aos="fade-right" data-aos-duration="1000">
+                <div class="text-2xl text-[#FE5B02]"><i class="fa-solid fa-laptop"></i></div>
+                <h3 class="text-xl font-bold my-2">Devlopement</h3>
+                <p class="text-sm text-[#565658]">
+                    Construct a customized solution according to your technical specifications.
+                </p>
+            </div>
+
+            <div class="sm:w-1/2 p-4" data-aos="fade-right" data-aos-duration="1000">
+                <div class="text-2xl text-[#FE5B02]"><i class="fa-solid fa-rocket"></i></div>
+                <h3 class="text-xl font-bold my-2">SEO optimisation</h3>
+                <p class="text-sm text-[#565658]">
+                    Audit your website, perform technical improvement and white meta tags.
+                </p>
+            </div>
+
+            <div class="sm:w-1/2 p-4" data-aos="fade-right" data-aos-duration="1000">
+                <div class="text-2xl text-[#FE5B02]"><i class="fa-solid fa-database"></i></div>
+                <h3 class="text-xl font-bold my-2">Data base</h3>
+                <p class="text-sm text-[#565658]">
+                    configure your data database server in sql server or mongodb server.
+                </p>
+            </div>
+
+            <div class="sm:w-1/2 flex flex-wrap items-center justify-center mt-8 lg:mt-0" data-aos="fade-right" data-aos-duration="1000">
+                <div class="mb-2 px-4 py-2 rounded bg-[#241F21] text-white text-xl">
+                    2 + Years experience
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <!-- Thech -->
+    <div class="pb-6 flex flex-wrap">
+        <h1 class="mb-8 mt-4 text-4xl w-full text-center" style="font-family: 'Barnacle Boy', sans-serif;" data-aos="flip-up" data-aos-duration="1000">Technologie i use </h1>
+
+        <div class="lg:w-3/4 mx-auto">
+            <div class="flex flex-wrap justify-center">
+
+                <div class="ml-2 mb-2 px-4 py-2 rounded bg-[#FD8D3E] text-white text-xl" data-aos="flip-right"
+                    data-aos-duration="500">JavaScript</div>
+                <div class="ml-2 mb-2 px-4 py-2 rounded bg-[#FE5B02] text-white text-xl" data-aos="flip-right"
+                    data-aos-duration="1000">VueJs</div>
+                <div class="ml-2 mb-2 px-4 py-2 rounded bg-[#e76c2a] text-white text-xl" data-aos="flip-right"
+                    data-aos-duration="1500">NodeJs</div>
+            </div>
+            <div class="flex flex-wrap justify-center">
+
+                <div class="ml-2 mb-2 px-4 py-2 rounded bg-[#FD8D3E] text-white text-xl" data-aos="flip-right"
+                    data-aos-duration="500">TypeScript</div>
+                <div class="ml-2 mb-2 px-4 py-2 rounded bg-[#252022] text-white text-xl" data-aos="flip-right"
+                    data-aos-duration="1000">PHP</div>
+                <div class="ml-2 mb-2 px-4 py-2 rounded bg-gray-600 text-white text-xl" data-aos="flip-right"
+                    data-aos-duration="1500">Laravel</div>
+                <div class="ml-2 mb-2 px-4 py-2 rounded bg-[#e76c2a] text-white text-xl" data-aos="flip-right"
+                    data-aos-duration="2000">ExpressJs</div>
+                <div class="ml-2 mb-2 px-4 py-2 rounded bg-[#E1E1E1] text-[#252022] text-xl" data-aos="flip-right"
+                    data-aos-duration="2500">Java</div>
+            </div>
+            <div class="flex flex-wrap justify-center">
+            <div class="ml-2 mb-2 px-4 py-2 rounded bg-[#FD8D3E] text-white text-xl" data-aos="flip-right"
+                data-aos-duration="500">MongoDb</div>
+            <div class="ml-2 mb-2 px-4 py-2 rounded bg-[#252022] text-white text-xl" data-aos="flip-right"
+                data-aos-duration="1000">SQL</div>
+        </div>
+    </div>
+</div></template>
